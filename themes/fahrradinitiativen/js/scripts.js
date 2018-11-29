f33676318eb23cd36400d6f4d35e8ac51dcd937f
Boris Hekele <boris@fixmyberlin.de>
2018-11-29 23:30:03 +0100

    Initial commit

---
+++ themes/fahrradinitiativen/js/scripts.js
@@ -0,0 +1,47 @@
+(function ($, Drupal) {
+
+    // Drupal behaviour function "attach"
+    // Called on page load and after ajax response to init js again
+    Drupal.behaviors.carousel = {
+        attach: function (context, settings) {
+            $(window).on("scroll", function () {
+                var fromTop = $(window).scrollTop();
+                $("body").toggleClass("scrolled", (fromTop > 150));
+            });
+            
+            var sticky = new Waypoint.Sticky({
+              element: $('#navbar')[0]
+            });
+
+            var bigSlideAPI = ($('.mobile-menu-trigger').bigSlide({
+                side: 'right',
+                menuWidth: "290px",
+                menu: '#menu-mobile',
+                easyClose: 'true'
+            })).bigSlideAPI;
+            $('.mobile-menu-close').click(function() {
+              bigSlideAPI.view.toggleClose();
+            });
+            
+            var bigSlideAPI2 = ($('.mobile-quicklinks-trigger').bigSlide({
+                side: 'left',
+                menuWidth: "290px",
+                menu: '#menu-quicklinks',
+                easyClose: 'true'
+            })).bigSlideAPI2;
+            $('.mobile-quicklinks-close').click(function() {
+              bigSlideAPI2.view.toggleClose();
+            });
+            
+            $(document).on('click', 'a.to-top', function(event){
+                event.preventDefault();
+
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 500);
+            });
+            
+        }
+    };
+
+})(jQuery, Drupal);
